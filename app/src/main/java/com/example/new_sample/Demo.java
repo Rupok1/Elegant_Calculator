9cdf0986ccfade9cc52ab25982733d0d237f3e83
Rupok1 <rupokhasan789@gmail.com>
2022-05-16 22:57:22 +0600

    Demo file added

---
+++ app/src/main/java/com/example/new_sample/Demo.java
@@ -0,0 +1,25 @@
+package com.example.new_sample;
+
+public class Demo {
+    public int id;
+    public String name;
+
+    public Demo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
