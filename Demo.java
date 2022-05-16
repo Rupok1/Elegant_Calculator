f36981336598717d02c1d4cc2cca470370c53c0e
Rupok1 <rupokhasan789@gmail.com>
2022-05-16 23:11:07 +0600

    Demo file created

--- Demo.java
+++ Demo.java
@@ -1,1 +1,25 @@
-Hello
+package com.example.new_sample;
+
+public class Demo {
+    public int id;
+    public String name;
+
+    public Demo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
